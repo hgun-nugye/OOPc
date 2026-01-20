56b6cb5d60a5f7e1951337ed8d880cecc1b1cfc7
Thanh Hương <thanhhuong10102005@gmail.com>
2026-01-20 20:55:54 +0700

    upload BTH2

---
+++ 2. BaiTH2/CheckCircle.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiThucHanh2
+{
+    public class Point
+    {
+        public int x;
+        public int y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+    }
+
+    public class Circle
+    {
+        public Point center;
+        public double radius;
+
+        public Circle(Point center, double radius=0)
+        {
+            this.center = center;
+            this.radius = radius;
+        }
+
+        public double Area()
+        {
+            return Math.PI * this.radius * this.radius;
+        }
+
+        public static Circle MaxCircle(List<Circle> circles)
+        {
+            double max = 0;
+            Circle c = null;;
+            foreach (Circle cir in circles)
+            {
+                double area = cir.Area();
+                if(area > max)
+                {
+                    max = area;
+                    c = cir;
+                }
+            }
+            return c;
+        }
+
+        public static double Distance(Point p1, Point p2)
+        {
+            return Math.Sqrt(Math.Pow(p1.x - p2.x, 2) +  Math.Pow(p1.y - p2.y, 2));
+        }
+
+        public static bool IntersectionCheck(Circle c1, Circle c2)
+        {
+            double d=Distance(c1.center, c2.center);
+            return (d < c1.radius + c2.radius);
+            
+        }
+    }
+
+
+    internal class CheckCircle
+    {
+        public static void Main()
+        {
+            List <Circle> circles = new List<Circle>();
+
+            double r=-1;
+            Console.WriteLine("Ket thuc nhap khi ban kinh = 0\n");
+            while (r!=0)
+            {
+                Console.WriteLine("Nhap vao ban kinh hinh tron: ");
+                r = double.Parse(Console.ReadLine());
+                if (r == 0) break;
+
+                Console.WriteLine("Nhap vao toa do tam hinh tron: ");
+                int x=int.Parse(Console.ReadLine());
+                int y=int.Parse(Console.ReadLine());
+                
+                circles.Add(new Circle(new Point(x,y),r));
+            }
+
+            Circle max = Circle.MaxCircle(circles);
+            if (max != null)
+            {
+                Console.WriteLine("\nHinh tron co dien tich lon nhat la ({0}, {1}) co ban kinh {2}\n", max.center.x, max.center.y, max.radius);
+            }
+
+            //kiểm tra giao nhau
+            int count=0;
+            for(int i=0; i<circles.Count-1; i++)
+            {
+                for(int j=i+1; j<circles.Count; j++)
+                {
+                    if (Circle.IntersectionCheck(circles[i], circles[j]))
+                    {
+                        Console.WriteLine("Hai hinh tron giao nhau!");
+                        Console.WriteLine("({0}, {1}) co ban kinh {2}", circles[i].center.x, circles[i].center.y, circles[i].radius);
+                        Console.WriteLine("({0}, {1}) co ban kinh {2}\n", circles[j].center.x, circles[j].center.y, circles[j].radius);
+                        count++;
+                    }
+                }
+            }
+            if(count==0) Console.WriteLine("Khong co hinh tron giao nhau!");
+        }
+    }
+}
---
+++ 2. BaiTH2/Point.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiThucHanh2
+{
+    public class Point
+    {
+        public double X {  get; set; }
+
+        public double Y { get; set; }
+
+        public Point(double x = 0, double y = 0)
+        {
+            X = x;
+            Y = y;
+        }
+        public void AddPoint()
+        {
+            Console.WriteLine("Nhap vao x: ");
+            X = double.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap vao y: ");
+            Y = double.Parse(Console.ReadLine());
+        }
+        public void DisplayPoint()
+        {
+            Console.WriteLine("({0};{1})", X, Y);
+        }
+        public void Move(double dx, double dy)
+        {
+            X += dx;
+            Y += dy;
+        }
+        public double Distance(Point p)
+        {
+            return Math.Sqrt(Math.Pow(this.X - p.X, 2) + Math.Pow(this.Y - p.Y, 2));
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Nhap so luong diem
+            int n;
+            do
+            {
+                Console.WriteLine("Nhap vao so luong diem: ");
+                n = int.Parse(Console.ReadLine());
+            }
+            while (n < 1);
+
+            //Nhap danh sach diem
+            Point[] listpoint = new Point[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Nhap vao diem thu {0}: ", i + 1);
+                listpoint[i] = new Point();
+                listpoint[i].AddPoint();
+            }
+
+            //In ra man hinh danh sach cac diem
+            Console.WriteLine("Danh sach cac diem vua nhap la: ");
+            foreach (Point p in listpoint)
+            {
+                p.DisplayPoint();
+            }
+
+            //Tim diem xa goc toa do nhat
+            Point GocToaDo = new Point(0, 0);
+            double maxDistance = listpoint[0].Distance(GocToaDo);
+            Point DiemXaNhat = listpoint[0];
+            foreach(Point p in listpoint)
+            {
+                if(p.Distance(GocToaDo) > maxDistance)
+                {
+                    maxDistance=p.Distance(GocToaDo);
+                    DiemXaNhat = p;
+                }
+            }
+            Console.WriteLine("Diem xa goc toa do nhat la: ");
+            DiemXaNhat.DisplayPoint();
+
+            //Tim cap diem gan nhau nhat
+            Point p1 = new Point();
+            Point p2 = new Point();
+            double DiemGanNhau = listpoint[1].Distance(listpoint[0]);
+            for(int i=0; i<n -1; i++)
+            {
+                for(int j=i+1; j<n; j++)
+                {
+                    if(listpoint[j].Distance(listpoint[i]) < DiemGanNhau)
+                    {
+                        DiemGanNhau = listpoint[j].Distance(listpoint[i]);
+                        p1 = listpoint[i];
+                        p2 = listpoint[j];
+                    }
+
+                }
+            }
+            Console.WriteLine("Cap diem gan nhau nhat la: ");
+            p1.DisplayPoint();
+            p2.DisplayPoint();
+
+        }
+    }
+}
